e410d34c04de8065926767d123b11fc1e5c129c1
Matthew <matthew.g.e123@gmail.com>
2025-12-31 16:25:46 +0000

    fix locale embed

--- GameLibrary.Logic/GameEmbeds/GameEmbed_Locale.cs
+++ GameLibrary.Logic/GameEmbeds/GameEmbed_Locale.cs
@@ -2,11 +2,12 @@ namespace GameLibrary.Logic.GameEmbeds;
 
 public class GameEmbed_Locale : IGameEmbed
 {
-    public int getPriority => 10;
+    public int getPriority => 0;
 
     public void Embed(RunnerManager.GameLaunchData inp, Dictionary<string, string?> args)
     {
-        inp.environmentArguments.Add("LANG", "ja_JP.UTF-8");
+        //inp.environmentArguments.Add("LANG", "ja_JP.UTF-8"); // breaks it?
+
         inp.environmentArguments.Add("LC_ALL", "ja_JP.UTF-8");
         inp.environmentArguments.Add("LC_CTYPE", "ja_JP.UTF-8");
     }
